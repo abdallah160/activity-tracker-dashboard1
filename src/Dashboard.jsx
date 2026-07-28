b13245f447c0a2dbb558d4d5ae7babc433e5c1bc
abdallah160 <s12324278@stu.najah.edu>
2026-07-28 15:31:28 +0300

    refactoring & database fixes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,74 +3,68 @@ import { activities } from "./activities";
 import { supabase } from "./supabaseClient";
 export default function Dashboard() {
     const [trackerData, setTrackerData] = useState([]);
-    const [avgData, setAvgData] = useState(handleOptionChange("weeks"));
+    const [avgData, setAvgData] = useState(activities);
     const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         async function fetchData() {
             setIsLoading(true);
-            const response = await supabase.from('tracker').select('*');
-            if (response.error) {
-                console.log(response.error)
-            }
-            else {
+            try {
+                const response = await supabase.from('tracker').select('*');
+                if (response.error) throw new Error();
                 setTrackerData(response.data)
-            };
+            }
+            catch (error) {
+                console.log(error);
+            }
+            finally {
+                setIsLoading(false);
+            }
             setIsLoading(false);
         }
-
         fetchData();
-
     }, []);
 
     useEffect(() => {
-        setAvgData(handleOptionChange("weeks"))
-
-
+        setAvgData(calculateTotals("weeks"))
     }, [trackerData])
 
-    function handleOptionChange(opt) {
+    function calculateTotals(opt) {
         if (trackerData.length < 1) return activities;
         const totals = structuredClone(activities);
         let interval;
-        if (opt == "weeks") {
+        if (opt === "weeks") {
             interval = 7;
         }
-        else if (opt == "months") {
+        else if (opt === "months") {
             interval = 30;
         }
-        else if (opt == "years") {
+        else if (opt === "years") {
             interval = 365;
         }
 
-        const dateIterator = new Date();
-        const aWeekAgo = new Date();
-        aWeekAgo.setDate(dateIterator.getDate() - interval);
-        while (dateIterator >= aWeekAgo) {
-
-            for (let dataObj of trackerData) {
-                const currentDate = dateIterator.toISOString().split("T")[0];
-                if (dataObj.date === currentDate) {
-                    totals[dataObj.activity] += dataObj.hours;
-                }
+        const startDate = new Date();
+        const today = new Date();
+        startDate.setDate(today.getDate() - interval);
+        for (let dataObj of trackerData) {
+            const dateValue = new Date(dataObj.date);
+            if (dateValue <= today && dateValue >= startDate) {
+                totals[dataObj.activity] += dataObj.hours;
             }
-            dateIterator.setDate(dateIterator.getDate() - 1);
-
         }
         return totals;
-
     }
+
+    let max = Math.max(...Object.values(avgData), 1);
     return (
         <div id="activity-page">
             <h3>Total hours spent on activites</h3>
             {isLoading && <p>fetching data...</p>}
             <div id="activity-view">
                 <div id="all-items">
-                    {Object.entries(avgData).map(([key, value], index) => {
-                        let max = Math.max(...Object.values(avgData), 1);
+                    {Object.entries(avgData).map(([key, value]) => {
                         let height = (value / max) * 300;
-
                         return (
-                            <div className="item" key={index}>
+                            <div className="item" key={key}>
                                 <div className="bar" style={{ height: `300px` }}>
                                     <div
                                         className="white"
@@ -87,7 +81,7 @@ export default function Dashboard() {
                         );
                     })}
                 </div>
-                <select onChange={(e) => setAvgData(handleOptionChange(e.target.value))}>
+                <select onChange={(e) => setAvgData(calculateTotals(e.target.value))}>
                     <option value="weeks">last week</option>
                     <option value="months">last month</option>
                     <option value="years">last year</option>
